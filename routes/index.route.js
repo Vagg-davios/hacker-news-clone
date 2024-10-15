1f707d6f1340d5069b8b2661213f37cb492db6a5
Vagg-Davios <evaggelosdavios@gmail.com>
2024-10-15 03:50:01 +0300

    add index route template and test for data delivery

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const items = ['Item 1', 'Item 2', 'Item 3'];
+    const items = ['A', 'B', 'C'];
     res.render('index', { items });
 });
 
